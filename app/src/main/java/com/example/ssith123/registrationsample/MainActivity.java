885f965ed7946f8d28301d2309e41634e278efcf
sanjeev <acharyabro123@gmail.com>
2017-03-21 16:05:04 +0545

    first commit

---
+++ app/src/main/java/com/example/ssith123/registrationsample/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.ssith123.registrationsample;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.ssith123.registrationsample.activity.LoginActivity;
+import com.example.ssith123.registrationsample.app.AppConfig;
+import com.example.ssith123.registrationsample.app.AppController;
+import com.example.ssith123.registrationsample.helper.SQLiteHandler;
+import com.example.ssith123.registrationsample.helper.SessionManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
+
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
+
+    }
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
---
+++ app/src/main/java/com/example/ssith123/registrationsample/app/AppConfig.java
@@ -0,0 +1,15 @@
+package com.example.ssith123.registrationsample.app;
+
+/**
+ * Created by SSITH123 on 3/20/2017.
+ */
+
+public class AppConfig {
+
+    // Server user login url
+    public static String URL_LOGIN = "http://10.0.2.2:8081/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://10.0.2.2:8081/android_login_api/register.php";
+
+}
